4b894a50441b89af0ed015e09945c451ef399476
Aron Tseggai <aron.tseggai@gmail.com>
2016-05-03 10:49:37 +0200

    Adds manager/apartments controller, you're now able to index and show

--- app/controllers/manager/apartments_controller.rb
+++ app/controllers/manager/apartments_controller.rb
@@ -1,2 +1,17 @@
 class Manager::ApartmentsController < ApplicationController
+  before_action :set_apartment, only: [:show]
+
+  def index
+    @apartments = Apartment.all
+
+  end
+
+  def show
+    set_apartment
+  end
+
+  private
+  def set_apartment
+    @apartment = Apartment.find(params[:id])
+  end
 end
